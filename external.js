3c9bfa1f6836956c5bdde01e650306408c381147
Gonzalo Silverio <gonzalo.silverio@gmail.com>
2017-10-04 07:56:54 -0400

    remove some fluff

--- external.js
+++ external.js
@@ -8,8 +8,7 @@ if (!($ = window.jQuery)) {
 }
 
 function startScraper() {
-
-  var localS = JSON.parse(localStorage.getItem('lessonData'));
+  var localS;
   if (!localStorage.getItem('lessonData')){
     localS = [];
   }
@@ -17,10 +16,7 @@ function startScraper() {
     localS = JSON.parse(localStorage.getItem('lessonData'));
   }
   var item = {type:'', content:'', choices:[], scale:[]};
-  var type;
-  var content;
 
-  //console.log($('table[cellpadding=2] p'));
   //multiple scale
   if ($('form > table > tbody> tr > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr').length > 0 &&
     $('form > table > tbody> tr > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody >tr:nth-child(1) > td').length > 0) {
@@ -36,9 +32,6 @@ function startScraper() {
     $('table[cellpadding=2] b').toArray().forEach(function(b) {
       item.scale.push(b.textContent);
     });
-
-//*[@id="main"]/form/table/tbody/tr/td/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td[1]
-
   }
 
   //poll
@@ -50,41 +43,44 @@ function startScraper() {
       }
     }
   }
+
   //single scale
   if ($('#main > form > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) td').length > 3 && $('#main > form > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) td').length === 0) {
     item.type = 'scale';
     item.content = $('#main > form > table > tbody > tr > td  > table  p').text();
   }
-  //short answer
 
+  //short answer
   if ($('input[onclick*="short_answer/reader"]').length) {
     item.type = 'short_answer';
     item.content = $('#main > form > table > tbody > tr > td p').text();
   }
+
   //true/false
   if ($('input[onclick*="true_false"]').length) {
     item.type = 'true_false';
     item.content = $('#main > form > table > tbody > tr > td  > p').text();
   }
+
   //multiple choice
   if ($('input[onclick*="multiple_choice"]').length) {
     item.type = 'multiple_choice';
     item.content = $('#main > form > table > tbody > tr > td  > p').html() + $('#main > form > table  > tbody > tr > td > table > tbody tr > td:nth-child(3)').text();
   }
+
   //multiple response
   if ($('input[onclick*="multiple_response"]').length) {
     item.type = 'multiple_response';
     item.content = $('#main > form > table > tbody > tr > td  > p').text() + $('#main > form > table > tbody > tr > td table p').text();
   }
+
   if ($('input[onclick*="short_answer/reader"]').length && $('#main > form > table > tbody > tr > td em').length) {
     item.type = 'fill_blanks';
     item.content = $('#main > form > table > tbody > tr > td p').html();
   }
 
-
   //if none of the above - it is a note
   item.type = item.type || 'note';
-
 
   if(item.type === 'note'){
     item.content = $('#main > form > table > tbody > tr > td p').text();
