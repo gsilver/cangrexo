f9cb35e00cea9e22a81a23b123b0e2f10e3fc187
Gonzalo Silverio <gonzalo.silverio@gmail.com>
2017-10-04 07:25:36 -0400

    add bkmarklet

---
+++ bkmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function() {
+	var js = document.createElement('script');
+	js.setAttribute('src', 'https://raw.githubusercontent.com/gsilver/cangrexo/master/external.js');
+	document.body.appendChild(js);
+}());
