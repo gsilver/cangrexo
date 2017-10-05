91cde75d344b25b96d444876dee943ed2c183105
Silverio <gsilver@umich.edu>
2017-10-05 09:52:44 -0400

    rename scrape js

--- bkmarklet.js
+++ bkmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function() {
 	var js = document.createElement('script');
-	js.setAttribute('src', 'https://raw.githubusercontent.com/gsilver/cangrexo/master/external.js');
+	js.setAttribute('src', 'https://raw.githubusercontent.com/gsilver/cangrexo/master/scrape.js');
 	document.body.appendChild(js);
 }());
